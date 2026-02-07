094bae5509bcdb70ab49f27a2b0cf111215ece11
Farhad <Farkhad0527@gmail.com>
2026-02-07 15:35:20 +0500

    Implement mobile menu toggle functionality
    Added event listeners for opening and closing the mobile menu, and implemented functionality to close the menu on scroll.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const sideOpener = document.querySelector('.side-opener');
+const IconHamburger = document.getElementById('icon-hamburger');
+const IconClose = document.getElementById('icon-close');
+const MobileMenu = document.getElementById('mobile-menu');
 
-const sidebarWindow = document.querySelector('.sidebar-window');
+// Open Mobile Menu
+IconHamburger.addEventListener('click', () => {
+    MobileMenu.classList.remove('translate-x-[100%]');
+    MobileMenu.classList.remove('pointer-events-none');
+    MobileMenu.classList.add('translate-x-0');
+    IconHamburger.classList.add('hidden');
+    IconClose.classList.remove('hidden');
+});
 
-const closeX = document.querySelector('.x');
+// Close Mobile Menu
+IconClose.addEventListener('click', () => {
+    MobileMenu.classList.remove('translate-x-0');
+    MobileMenu.classList.add('pointer-events-none');
+    MobileMenu.classList.add('translate-x-[100%]');
+    IconHamburger.classList.remove('hidden');
+    IconClose.classList.add('hidden');
+});
 
-const hamBurger = document.querySelector('.hamburger');
-
-sideOpener.onclick = () => {
-    sidebarWindow.classList.toggle('open');
-    closeX.classList.toggle('d-none');
-    hamBurger.classList.toggle('d-none');
-};
+// Close Mobile Menu when scroll
+window.onscroll = () => {
+    if (window.scrollY > 0) {
+        MobileMenu.classList.remove('translate-x-0');
+        MobileMenu.classList.add('translate-x-[100%]');
+        IconHamburger.classList.remove('hidden');
+        IconClose.classList.add('hidden');
+    }
+}
